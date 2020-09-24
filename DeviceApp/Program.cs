7d24882cd6b583b144a67009c3b1bb662f8d5608
Jesper Müllern <mullern23@hotmail.com>
2020-09-24 12:04:56 +0200

    Add project files.

---
+++ DeviceApp/Program.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceApp
+{
+    class Program
+    {
+        private static DeviceClient deviceClient = DeviceClient.CreateFromConnectionString("HostName=WIN20-iothub.azure-devices.net;DeviceId=DeviceApp;SharedAccessKey=Qtj9zuTHh1aF95Fa+4LY/5k7rTwXJiysCvGVGbnulL4=", TransportType.Mqtt);
+        private static int telemetryInterval = 5;
+        private static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            deviceClient.SetMethodHandlerAsync("SetTelemetryInterval", SetTelemetryInterval, null).Wait();
+            SendMessageAsync().GetAwaiter();
+
+            Console.ReadKey();
+        }
+
+        private static Task<MethodResponse> SetTelemetryInterval(MethodRequest request, object userContext)
+        {
+            var payload = Encoding.UTF8.GetString(request.Data); //.Replace("\"","")
+
+            if (Int32.TryParse(payload, out telemetryInterval))
+            {
+                Console.WriteLine($"Interval set to: {telemetryInterval} seconds.");
+
+                // { "result": "Executed direct method: SetTelemetryInterval" }
+                string json = "{\"result\": \"Executed direct method: " + request.Name + "\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 200));
+            }
+            else
+            {
+                string json = "{\"result\": \"Method not implemented\"}";
+                return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(json), 501)); //400 eller 501 (använda 501 i detta fall)
+            }
+
+        }
+
+        private static async Task SendMessageAsync()
+        {
+            while (true)
+            {
+                double temp = 10 + rnd.NextDouble() * 15;
+                double hum = 40 + rnd.NextDouble() + 20;
+
+                var data = new
+                {
+                    temperature = temp,
+                    humidity = hum
+                };
+                var json = JsonConvert.SerializeObject(data);
+                var payload = new Message(Encoding.UTF8.GetBytes(json));
+                payload.Properties.Add("temperatureAlert", (temp > 30) ? "true" : "false");
+
+                await deviceClient.SendEventAsync(payload);
+                Console.WriteLine($"Message sent: {json}");
+
+                await Task.Delay(telemetryInterval * 1000);
+            }          
+        }
+    }
+}
---
+++ ServiceApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Azure.Devices;
+using System;
+using System.Threading.Tasks;
+
+namespace ServiceApp
+{
+    class Program
+    {
+        private static ServiceClient serviceClient = ServiceClient.CreateFromConnectionString("HostName=WIN20-iothub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=VpiWQr5A3vMxsO2IaH5/rwcw49EPCqEHJhPZFOWZtlw=");
+
+        static void Main(string[] args)
+        {
+            Task.Delay(5000).Wait();
+
+            InvokeMethod("DeviceApp", "SetTelemetryInterval", "10").GetAwaiter();
+            Console.ReadKey();
+        }
+
+        static async Task InvokeMethod(string deviceId, string methodName, string payload)
+        {
+            var methodInvocation = new CloudToDeviceMethod(methodName) { ResponseTimeout = TimeSpan.FromSeconds(30) };
+            methodInvocation.SetPayloadJson(payload);
+
+            var response = await serviceClient.InvokeDeviceMethodAsync(deviceId, methodInvocation);
+            Console.WriteLine($"Response Status: {response.Status}");
+            Console.WriteLine(response.GetPayloadAsJson());
+
+        }
+
+
+
+
+    }
+}
